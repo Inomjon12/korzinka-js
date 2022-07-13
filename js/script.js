7f31c064ea0d495d89db062b7264a1361a54bac5
Inomjon12 <inomjonismanaliev@gmail.com>
2022-07-13 22:46:51 +0500

    Add site creatCart in js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ window.addEventListener("DOMContentLoaded", function () {
   let product = document.querySelectorAll(".product"),
     buttons = document.querySelectorAll("button"),
     openBtn = document.querySelector(".open");
+
+  function creatCart() {
+    let cart = document.createElement("div");
+    field = document.createElement("div");
+    heading = document.createElement("h2");
+	  closeBtn = document.createElement("button");
+  }
+  creatCart();
 });
