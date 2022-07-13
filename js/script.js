c5f2982b616de095a4ba205e3e0327b308d96c7d
Inomjon12 <inomjonismanaliev@gmail.com>
2022-07-13 22:43:03 +0500

    Add site DOMContentLoaded in js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-let product = document.querySelectorAll(".product"),
-  buttons = document.querySelectorAll("button"),
-  openBtn = document.querySelector(".open");
+window.addEventListener("DOMContentLoaded", function () {
+  let product = document.querySelectorAll(".product"),
+    buttons = document.querySelectorAll("button"),
+    openBtn = document.querySelector(".open");
+});
